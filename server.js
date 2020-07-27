a38ee80f52262d015cdf309435248bb25e5f6309
cwirl <cwilmer@uninorte.edu.co>
2020-07-27 13:26:42 -0500

    chore: api error handling

--- server.js
+++ server.js
@@ -9,5 +9,33 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use("/api", routes);
 
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+if (process.env.NODE_ENV !== "production") {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
 const port = PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
